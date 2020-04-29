c6804f189e5228889104567438b0fb5279248f0c
wangwenlong <357934522@qq.com>
2020-04-29 10:35:50 +0800

    .

---
+++ main.js
@@ -0,0 +1,145 @@
+let pu = require('./pUtils')
+let axios = require('axios')
+let fs = require('fs')
+let moment = require('moment');
+let { userName, password } = require('./config.js');
+
+class M extends pu {
+    constructor() {
+        super();
+        this.taskName = 'mashi'
+        this.cookie = `_139_login_version=25; UUIDToken=c563c484-0525-4522-be69-92feb7170b39; _139_index_isSmsLogin=1; a_l=1585024975000|1887718527; a_l2=1585024975000|12|MTUwMTEwOTkzMTJ8MjAyMC0wMy0yNCAxMjo0Mjo1NXxiZEpzSmp2WG1MUko1T2MzOFBVd2UvWnN4ZktZMkRpME8rTnZ6R1pDQTFzPXxlZjViMTEyYTk2NTIxYjIzMmUwOWU2MmI2MTg2OGJjNQ==; RMKEY=f7f8ce53754c8727; Os_SSo_Sid=00U2OTQ3Mjk3NTAwMTk0NDU005087B73000001; cookiepartid3124=12; cookiepartid=12; Login_UserNumber=15011099312; UserData={}; SkinPath23124=; rmUin3124=1336440719; provCode3124=6; areaCode3124=600; loginProcessFlag=`;
+        this.list = [];
+        this.init();
+        this.sendSmsTryCount = 0;
+    }
+
+    async init() {
+        this.list = JSON.parse(fs.readFileSync('./data.json').toString() || '""') || []
+    }
+
+    async sendSMS(content = '666') {
+        try {
+            this.log(`>>>sendSMS:${this.sendSmsTryCount}`);
+            this.sendSmsTryCount += 1;
+            if (this.sendSmsTryCount > 5) return false;
+            await this.runPuppeteer({
+                headless: true
+            });
+            await this.sleep(1000)
+            let url = `http://mail.10086.cn/`
+            this.page.goto(url, {
+                waitUntil: 'domcontentloaded'
+            });
+            await this.page.waitForSelector('#J_loginFormHeight');
+            await this.page.type('#txtUser', userName)
+            await this.page.type('#txtPass', password)
+            await this.sleep()
+            await this.page.click('#loginBtn')
+            await this.page.waitForSelector('#welcome');
+            await this.sleep(500)
+            this.pageSMS = this.page.frames().find(frame => frame.url().indexOf('m2012server/home') > -1)
+            await this.pageSMS.waitForSelector('#smsSend');
+            await this.pageSMS.click('#smsSend')
+            await this.sleep(500)
+            await this.page.reload()
+            await this.page.waitForSelector('#divTab');
+            await this.sleep()
+            this.pageSend = this.page.frames().find(frame => frame.url().indexOf('sms/sms_send') > -1)
+            // await this.pageSend.click('#txtMobiles')
+            await this.sleep()
+            await this.pageSend.click('.PlaceHold')
+            await this.sleep(500)
+            await this.pageSend.type('.addrText-input', '15011099312', {
+                delay: 200
+            })
+            await this.sleep()
+            await this.pageSend.click('#txtContent')
+            await this.sleep()
+            await this.pageSend.type('#txtContent', `码市:${content}\n${moment().format('YYYY-MM-DD HH:mm:ss')}\n`, {
+                delay: 20
+            })
+            await this.sleep(500)
+            this.pageSend.click('#btnSmsSendFooter a')
+            await this.sleep(2000)
+            await this.closePuppeteer();
+        } catch (err) {
+            this.log(err);
+            await this.sleep(1000 * 10)
+            await this.closePuppeteer();
+            await this.sendSMS(content)
+        }
+    }
+
+
+    sleep(ms = 300) {
+        return new Promise(async (resolve, reject) => {
+            setTimeout(_ => {
+                resolve()
+            }, ms)
+        })
+    }
+
+    async getData() {
+        this.log(`>>>getData`)
+        return new Promise(async (resolve, reject) => {
+            let url = 'https://codemart.com/api/project?page=1&roleTypeId='
+            let cookie = `_ga=GA1.2.1458977849.1562917775; mid=812deff9-e926-428c-b1d7-24c15465dc30; exp=89cd78c2; withGlobalSupport=true; user-vip=true; _gid=GA1.2.1817827069.1569467425; _gat=1`
+            axios.get(url, {
+                header: {
+                    cookie,
+                },
+                data: {
+                    page: 1,
+                }
+            }).then(res => {
+                let rewards = res.data.rewards;
+                if (rewards && rewards.length) {
+                    let ownerIdArr = res.data.rewards.map(item => item.ownerId).splice(0, 5);
+                    //查询接口里有的数据，list没有
+                    let newIds = [];
+                    ownerIdArr.map(id => {
+                        if (!this.list.includes(id)) {
+                            newIds.push(id)
+                        }
+                    })
+                    if (newIds.length) {
+                        this.list = [...this.list, ...newIds]
+                        fs.writeFileSync('./data.json', JSON.stringify(this.list))
+                    }
+                    resolve(newIds)
+                } else {
+                    reject();
+                }
+
+            }).catch(err => {
+                this.log(err);
+                reject(err)
+            })
+        })
+
+    }
+
+    async task1() {
+        do {
+            this.log(`>>>do`);
+            try {
+                let ids = await this.getData();
+                if (ids.length) {
+                    this.sendSmsTryCount = 0;
+                    await this.sendSMS(ids[0])
+                }
+            } catch (err) {
+                this.log(err);
+            }
+            await this.sleep(4000)
+        } while (true)
+    }
+
+    async main() {
+        this.log(`>>>main`)
+        this.task1()
+    }
+}
+
+new M().main();
---
+++ pUtils.js
@@ -0,0 +1,206 @@
+let puppeteer = require('puppeteer');
+let fs = require('fs')
+let moment = require('moment');
+
+
+
+class PU {
+  constructor() {
+
+  }
+
+  log(T) {
+    let info = ''
+    try {
+      if (T instanceof Error) {
+        console.error(T)
+        info = T.message
+        if (info != '修改保存异常') {
+          debugger;
+        }
+      } else {
+        info = JSON.stringify(T).replace(/^\"+/, '').replace(/\"+$/, '')
+      }
+      try {
+        info = info.replace(/\\n/g, '').replace(/\s+/g, ' ')
+      } catch (err) {
+        console.log(err);
+      }
+      info = moment().format('YYYY-MM-DD HH:mm:ss') + ' ' + info
+      console.log(info);
+      if (this.taskName) {
+        if (!fs.existsSync(`./logs/${this.taskName}/`)) {
+          fs.mkdirSync(`./logs/${this.taskName}/`)
+        }
+        fs.appendFileSync(`./logs/${this.taskName}/${moment().format('YYYY-MM-DD')}.log`, info +
+          '\n')
+      } else {
+        fs.appendFileSync(`./logs/${moment().format('YYYY-MM-DD')}.log`, info + '\n')
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  sleep(ms = 300) {
+    this.log(`>>>sleep:${ms}`)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, ms);
+    })
+  }
+
+
+  async runPuppeteer(options = {}) {
+    this.log(`>>>runPuppeteer`);
+    this.browser = await puppeteer.launch(Object.assign({}, {
+      headless: false,
+      args: ['--start-maximized', '--disable-infobars']
+    }, options));
+    this.page = await this.browser.newPage();
+    await this.page.setViewport({
+      width: 1200,
+      height: 800
+    })
+    return {
+      browser: this.browser,
+      page: this.page
+    }
+  }
+
+  async goto(url, selector, page = this.page) {
+    await page.goto(url, {
+      waitUntil: 'domcontentloaded'
+    })
+    if (selector) {
+      await waitForSelector(selector)
+    }
+  }
+
+  async closePuppeteer() {
+    this.log('>>>closePuppeteer');
+    try {
+      if (this.browser) {
+        await this.browser.close()
+      }
+    } catch (error) {
+      this.log(error)
+    }
+  }
+
+  async loadJquery(page = this.page) {
+    try {
+      await page.evaluate(() => {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '//cdn.bootcss.com/jquery/2.1.2/jquery.min.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+      })
+      await this.sleep(1000)
+    } catch (err) {
+      this.log(err)
+    }
+  }
+
+  /**
+   * 查找等待元素出现
+   * @param {*} selector
+   * @param {*} page
+   */
+  async waitElement(selector, page = this.page) {
+    let len = 0;
+    try {
+      this.log('>>>waitElement');
+      this.log(selector)
+      let jqueryExist = false;
+      let doCount = 0
+      do {
+        doCount++;
+        this.log(`do:jqueryExist:${jqueryExist}`)
+        await this.sleep()
+        jqueryExist = await page.evaluate(() => {
+          return typeof window.jQuery === 'function'
+        })
+        if (doCount > 20) {
+          doCount = 0;
+          await this.loadJquery(page);
+        }
+      } while (!jqueryExist)
+
+      for (let index = 0; index < 10; index++) {
+        this.log(`waitElement第${index}次寻找...`)
+        await this.sleep(500)
+        len = await page.evaluate(selector => {
+          return jQuery(selector).length;
+        }, selector);
+        this.log(`寻找结果${len}`)
+        if (len) {
+          break;
+        }
+      }
+    } catch (error) {
+      this.log(error)
+    }
+    return len;
+  }
+
+
+
+  /**
+   * 等待jquery
+   */
+  async waitJquery(page = this.page) {
+    this.log(`>>>waitJquery`)
+    let jqueryExist = false;
+    let doCount = 0;
+    do {
+      doCount++;
+      this.log(`do:jqueryExist:${jqueryExist}`)
+      await sleep()
+      jqueryExist = await page.evaluate(() => {
+        return typeof window.jQuery === 'function'
+      })
+
+      if (doCount > 20) {
+        doCount = 0;
+        await this.loadJquery(page);
+      }
+    } while (!jqueryExist)
+  }
+
+
+
+  unique(arr) {
+    return Array.from(new Set(arr))
+  }
+
+
+
+
+  async setCookie(cookies_str = "", domain, page = this.page) {
+    this.log(`>>>setCookie`);
+    let cookies = cookies_str.split(';').map(pair => {
+      let name = pair.trim().slice(0, pair.trim().indexOf('='))
+      let value = pair.trim().slice(pair.trim().indexOf('=') + 1)
+      return {
+        name,
+        value,
+        domain
+      }
+    });
+    return Promise.all(cookies.map(pair => {
+      return page.setCookie(pair)
+    }));
+  }
+
+  async setPageCookie(session, page = this.page) {
+    await this.setCookie(session, '.58ganji.com', page);
+    await this.setCookie(session, '.58.com', page);
+    await this.setCookie(session, '.vip.58.com', page);
+    await this.setCookie(session, '.anjuke.com', page);
+    await this.setCookie(session, '.vip.58ganji.com', page);
+  }
+
+}
+module.exports = PU;
